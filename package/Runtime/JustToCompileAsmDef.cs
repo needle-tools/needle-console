12f57e26256ea1fce36eb0694ef2ce61913b273f
Felix Herbst <felix.herbst@gmail.com>
2021-04-27 15:40:15 +0200

    rm Runtime folder for now, Editor-only usage

--- package/Runtime/JustToCompileAsmDef.cs
+++
@@ -1,8 +0,0 @@
-namespace Needle.Demystify
-{
-    // ReSharper disable once UnusedType.Global
-    internal class JustToCompileAsmDef
-    {
-        // Just here for assembly def to compile 
-    }
-}
