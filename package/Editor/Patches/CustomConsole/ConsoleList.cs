f1ca3b9717206dca22946c0d63233c2513f147f5
Marcel Wiessler <marcel@gaisterhand.de>
2021-05-06 23:41:08 +0200

    basic console functionality is restored and test with exclude by file via context menu

--- package/Editor/Patches/CustomConsole/ConsoleList.cs
+++ package/Editor/Patches/CustomConsole/ConsoleList.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
@@ -8,8 +10,14 @@ namespace Needle.Demystify
 {
 	internal static class ConsoleList
 	{
-
 		private static string filter;
+		private static List<string> excludeFiles = new List<string>();
+
+		private static bool custom
+		{
+			get => SessionState.GetBool("ConsoleListCustom", false);
+			set => SessionState.SetBool("ConsoleListCustom", value);
+		}
 
 		private class TempFilterWindow : EditorWindow
 		{
@@ -23,6 +31,9 @@ namespace Needle.Demystify
 			private void OnGUI()
 			{
 				EditorGUI.BeginChangeCheck();
+
+				custom = EditorGUILayout.Toggle("Draw Custom", custom);
+				
 				filter = EditorGUILayout.TextField(filter);
 
 				if (EditorGUI.EndChangeCheck())
@@ -34,13 +45,24 @@ namespace Needle.Demystify
 		
 		
 		
-		private static Vector2 scroll;
-		private static readonly List<(LogEntry, string str)> currentEntries = new List<(LogEntry, string)>();
+		private static Vector2 scroll, scrollStacktrace;
+		private static readonly List<(LogEntry entry, string str, int row)> currentEntries = new List<(LogEntry, string, int)>();
+		private static SplitterState spl = SplitterState.FromRelative(new float[] {70, 30}, new float[] {32, 32}, null);
 
-		internal static void OnDrawList()
+		private static int selectedRow = -1, previouslySelectedRow = -2, rowDoubleClicked = -1;
+		private static string selectedText;
+
+		private static int collapsedFlag = 1 << 0;
+		private static bool HasFlag(int flags) { return (LogEntries.consoleFlags & (int)flags) != 0; }
+
+		internal static bool OnDrawList(ConsoleWindow console)
 		{
+			if(!custom)
+				return true;
+			
 			// scroll = EditorGUILayout.BeginScrollView(scroll);
 			var count = 0;
+			var collapsed = HasFlag(collapsedFlag);
 
 			if (Event.current.type == EventType.Repaint)
 			{
@@ -55,18 +77,21 @@ namespace Needle.Demystify
 						var preview = default(string);
 						LogEntries.GetLinesAndModeFromEntryInternal(i, ConsoleWindow.Constants.LogStyleLineCount, ref mask, ref preview);
 
-						var entry = new LogEntry();
-						LogEntries.GetEntryInternal(i, entry);
 
 						if (!string.IsNullOrWhiteSpace(filter) && !preview.Contains(filter))
 						{
-							// preview += " filtered";
 							continue;
 						}
 						
+						var entry = new LogEntry();
+						LogEntries.GetEntryInternal(i, entry); 
+
+						if (excludeFiles.Count > 0 && excludeFiles.Contains(entry.file))
+						{
+							continue;
+						}
 						
-						var c = currentEntries.Count;
-						currentEntries.Add((entry, preview + ", " + c));
+						currentEntries.Add((entry, preview, i));
 					}
 				}
 				finally
@@ -74,49 +99,111 @@ namespace Needle.Demystify
 					LogEntries.EndGettingEntries();
 				}
 			}
+			
+			SplitterGUILayout.BeginVerticalSplit(spl);
 
 			var yTop = EditorGUIUtility.singleLineHeight + 3;
-			var lineHeight = EditorGUIUtility.singleLineHeight * 1.2f;
+			var lineHeight = EditorGUIUtility.singleLineHeight * 1.1f;
 			count = currentEntries.Count;
+			var scrollHeight = Screen.height - spl.realSizes[1] - 44;
 			var contentHeight = count * lineHeight;
-			var scrollArea = new Rect(0, yTop, Screen.width - 3, Screen.height * .5f);
+			var scrollArea = new Rect(0, yTop, Screen.width - 3, scrollHeight);
 			var width = Screen.width - 3;
 			if(contentHeight > scrollArea.height)
 				width -= 13;
 			var contentSize = new Rect(0, 0, width, contentHeight);
+			
 			scroll = GUI.BeginScrollView(scrollArea, scroll, contentSize);
 			var position = new Rect(0, 0, width, lineHeight);
 			var element = new ListViewElement();
 			var style = new GUIStyle(ConsoleWindow.Constants.LogSmallStyle);
 			style.alignment = TextAnchor.MiddleLeft;
 			var strRect = new Rect(position);
-			strRect.height -= position.height * .18f;
+			strRect.height -= position.height * .15f;
+			var tempContent = new GUIContent();
+			
 			try
 			{
+				if (Event.current.type == EventType.MouseDown)
+				{
+					rowDoubleClicked = -1;
+					previouslySelectedRow = selectedRow;
+					selectedRow = int.MaxValue;
+				}
+				
 				LogEntries.StartGettingEntries();
 				for (var k = 0; k < currentEntries.Count; k++)
 				{
 					if (Event.current.type == EventType.Repaint)
 					{
 						var row = k;
+						var entry = currentEntries[k];
+						var isSelected = entry.row == selectedRow;
 
-						if (row % 2 == 0)
+						if (row % 2 == 0 || isSelected)
 						{
 							var prevCol = GUI.color;
-							GUI.color = new Color(0, 0, 0, .1f);
+							GUI.color = isSelected ? new Color(.15f, .4f, .5f) : new Color(0, 0, 0, .1f);
 							GUI.DrawTexture(position, Texture2D.whiteTexture);
 							GUI.color = prevCol;
 						}
 
 						element.row = row;
 						element.position = position;
-						var entry = currentEntries[k];
 						var preview = entry.str;
+						// preview += spl.realSizes[0] + ", " + spl.realSizes[1];
 						ConsoleText.ModifyText(element, ref preview);
 						GUI.Label(strRect, preview, style);
-						position.y += lineHeight;
-						strRect.y += lineHeight;
+						
+
+						if (collapsed)
+						{
+							Rect badgeRect = element.position;
+							tempContent.text = LogEntries.GetEntryCount(entry.row)
+								.ToString(CultureInfo.InvariantCulture);
+							Vector2 badgeSize = ConsoleWindow.Constants.CountBadge.CalcSize(tempContent);
+
+							if (ConsoleWindow.Constants.CountBadge.fixedHeight > 0)
+								badgeSize.y = ConsoleWindow.Constants.CountBadge.fixedHeight;
+							badgeRect.xMin = badgeRect.xMax - badgeSize.x;
+							badgeRect.yMin += ((badgeRect.yMax - badgeRect.yMin) - badgeSize.y) * 0.5f;
+							badgeRect.x -= 5f;
+							GUI.Label(badgeRect, tempContent, ConsoleWindow.Constants.CountBadge);
+						}
 					}
+					else if (Event.current.type == EventType.MouseUp)
+					{
+						if (Event.current.button == 0)
+						{
+							if (position.Contains(Event.current.mousePosition))
+							{
+								selectedRow = currentEntries[k].row;
+								selectedText = currentEntries[k].entry.message;
+								if (previouslySelectedRow == selectedRow) 
+									rowDoubleClicked = selectedRow;
+								console.Repaint();
+							}
+						}
+						else if (Event.current.button == 1)
+						{
+							if (position.Contains(Event.current.mousePosition))
+							{
+								var item = currentEntries[k];
+								var menu = new GenericMenu();
+								var fileName = Path.GetFileName(item.entry.file);
+								menu.AddItem(new GUIContent("Exclude " + fileName), false, () =>
+								{
+									excludeFiles.Add(item.entry.file);
+									console.Repaint();
+								});
+								menu.ShowAsContext();
+							}
+
+						}
+					}
+					
+					position.y += lineHeight;
+					strRect.y += lineHeight;
 				}
 			}
 			finally
@@ -124,8 +211,28 @@ namespace Needle.Demystify
 				LogEntries.EndGettingEntries();
 			}
 
-			// EditorGUILayout.EndScrollView();
+			if (rowDoubleClicked >= 0)
+			{
+				LogEntries.RowGotDoubleClicked(rowDoubleClicked);
+				rowDoubleClicked = -1;
+				previouslySelectedRow = -1;
+			}
+
 			GUI.EndScrollView();
+			
+			// Display active text (We want word wrapped text with a vertical scrollbar)
+			GUILayout.Space(scrollHeight+2);
+			scrollStacktrace = GUILayout.BeginScrollView(scrollStacktrace, ConsoleWindow.Constants.Box);
+
+			var stackWithHyperlinks = ConsoleWindow.StacktraceWithHyperlinks(selectedText ?? string.Empty);
+			var height = ConsoleWindow.Constants.MessageStyle.CalcHeight(GUIContent.Temp(stackWithHyperlinks), position.width);
+			EditorGUILayout.SelectableLabel(stackWithHyperlinks, ConsoleWindow.Constants.MessageStyle, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true), GUILayout.MinHeight(height + 10));
+
+			GUILayout.EndScrollView();
+			
+			SplitterGUILayout.EndVerticalSplit();
+
+			return false;
 		}
 	}
 }
--- package/Editor/Patches/CustomConsole/Patch_ConsoleWindowListView.cs
+++ package/Editor/Patches/CustomConsole/Patch_ConsoleWindowListView.cs
@@ -7,6 +7,7 @@ using HarmonyLib;
 using needle.EditorPatching;
 using UnityEditor;
 using UnityEngine;
+
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMember.Local
 
@@ -36,103 +37,107 @@ namespace Needle.Demystify
 			}
 
 			private static IEnumerable<CodeInstruction> Transpiler(MethodBase method, ILGenerator il, IEnumerable<CodeInstruction> instructions)
-			{ 
-				
-				// var skipLabel = il.DefineLabel();
-
+			{
+				var skipLabel = il.DefineLabel();
 				var arr = instructions.ToArray();
-				// CodeInstruction enumeratorInstruction = null;
-				
 				var loadListViewElementIndex = -1;
-				var customDraw = true;
-				
+
 				for (var index = 0; index < arr.Length; index++)
 				{
 					var inst = arr[index];
-					
-					if(customDraw && index >= 319 && index <= 982) continue;
+
+					// if(customDraw && index >= 319 && index <= arr.Length-1) continue;
 					// if(index > 350 && index < 500)
-						// Debug.Log("<color=grey>" + index + ": " + inst + "</color>");
+					// Debug.Log("<color=grey>" + index + ": " + inst + "</color>");
 
 					// get local index for current list view element
 					if (loadListViewElementIndex == -1 || inst.IsStloc() && inst.operand is LocalBuilder)
 					{
 						var loc = inst.operand as LocalBuilder;
-						if(loc?.LocalType == typeof(ListViewElement))
+						if (loc?.LocalType == typeof(ListViewElement))
 							loadListViewElementIndex = loc.LocalIndex;
 					}
-					
+
 					if (inst.opcode == OpCodes.Call && inst.operand is MethodInfo m)
 					{
 						if (m.DeclaringType == typeof(LogEntries) && m.Name == "GetLinesAndModeFromEntryInternal")
-						{ 
+						{
 							yield return inst;
 							// load text is one element before
-							var ldStr = arr[index - 1]; 
+							var ldStr = arr[index - 1];
 							yield return new CodeInstruction(OpCodes.Ldloc, loadListViewElementIndex);
 							yield return ldStr;
 							yield return CodeInstruction.Call(typeof(ConsoleText), nameof(ConsoleText.ModifyText));
-							continue; 
+							continue;
 						}
 					}
 
-					yield return inst; 
-					
-					if (customDraw && index == 318)
+					if (inst.opcode == OpCodes.Ret)
 					{
-						yield return CodeInstruction.Call(typeof(ConsoleList), nameof(ConsoleList.OnDrawList));
+						inst.labels.Add(skipLabel);
 					}
-				}
-			}
-		}
 
-		private class EnumeratorPatch : EditorPatch
-		{
-			protected override Task OnGetTargetMethods(List<MethodBase> targetMethods)
-			{
-				var method = typeof(ListViewShared.ListViewElementsEnumerator).GetMethod("MoveNext");
-				targetMethods.Add(method);
-				return Task.CompletedTask;
-			}
-
-			private static int count;
-
-			private static void Prefix(int ___xPos)
-			{
-				if (___xPos <= -1)
-					count = 0;
-			}
-			
-			// https://github.com/Unity-Technologies/UnityCsReference/blob/61f92bd79ae862c4465d35270f9d1d57befd1761/Editor/Mono/GUI/ListViewShared.cs#L411
-			private static void Postfix(ListViewShared.ListViewElementsEnumerator __instance, ref bool __result, ref ListViewElement ___element, ListViewShared.InternalLayoutedListViewState ___ilvState)
-			{
-				if (__result)
-				{
-					if (___element.row == 0) count = 0;
-					
-					if (___element.row % 2 == 0)
+					if (index == 318)
 					{
-						count += 1;
-						// ___element.position.height = 0;
-						___element.row += 1;
-						// if(___ilvState != null && ___ilvState.@group != null)
-						// 	___ilvState.@group.AddY(-___ilvState.rectHeight);
-						// __instance.MoveNext();
+						yield return new CodeInstruction(OpCodes.Ldarg_0);
+						yield return CodeInstruction.Call(typeof(ConsoleList), nameof(ConsoleList.OnDrawList));
+						yield return new CodeInstruction(OpCodes.Brfalse, skipLabel);
 					}
 					else
 					{
-						count += 1;
-						___element.row += count;
-						if (___element.row >= ___ilvState.state.totalRows) __result = false;
-
-						// var el = ___element.position;
-						// el.y -= el.height * count;
-						// ___element.row -= count;
-						// ___element.row = Mathf.Max(___element.row, 0);
-						// ___element.position = el;
+						yield return inst;
 					}
 				}
 			}
 		}
+
+		// private class EnumeratorPatch : EditorPatch
+		// {
+		// protected override Task OnGetTargetMethods(List<MethodBase> targetMethods)
+		// {
+		// 	var method = typeof(ListViewShared.ListViewElementsEnumerator).GetMethod("MoveNext");
+		// 	targetMethods.Add(method);
+		// 	return Task.CompletedTask;
+		// }
+		//
+		// private static int count;
+		//
+		// private static void Prefix(int ___xPos)
+		// {
+		// 	if (___xPos <= -1)
+		// 		count = 0;
+		// }
+
+		// // https://github.com/Unity-Technologies/UnityCsReference/blob/61f92bd79ae862c4465d35270f9d1d57befd1761/Editor/Mono/GUI/ListViewShared.cs#L411
+		// private static void Postfix(ListViewShared.ListViewElementsEnumerator __instance, ref bool __result, ref ListViewElement ___element, ListViewShared.InternalLayoutedListViewState ___ilvState)
+		// {
+		// 	if (__result)
+		// 	{
+		// 		if (___element.row == 0) count = 0;
+		// 		
+		// 		if (___element.row % 2 == 0)
+		// 		{
+		// 			count += 1;
+		// 			// ___element.position.height = 0;
+		// 			___element.row += 1;
+		// 			// if(___ilvState != null && ___ilvState.@group != null)
+		// 			// 	___ilvState.@group.AddY(-___ilvState.rectHeight);
+		// 			// __instance.MoveNext();
+		// 		}
+		// 		else
+		// 		{
+		// 			count += 1;
+		// 			___element.row += count;
+		// 			if (___element.row >= ___ilvState.state.totalRows) __result = false;
+		//
+		// 			// var el = ___element.position;
+		// 			// el.y -= el.height * count;
+		// 			// ___element.row -= count;
+		// 			// ___element.row = Mathf.Max(___element.row, 0);
+		// 			// ___element.position = el;
+		// 		}
+		// 	}
+		// }
+		// }
 	}
 }
