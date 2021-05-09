1753c0544266023293cc790808d76b170a7796a2
Marcel Wiessler <marcel@gaisterhand.de>
2021-05-09 18:06:29 +0200

    log many test

--- projects/needle.demystify.extras/DemystifyTestCode/Logging1.cs
+++ projects/needle.demystify.extras/DemystifyTestCode/Logging1.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
@@ -7,26 +8,25 @@ namespace Demystify._Tests
 {
 	public class Logging
 	{
-		
 		[MenuItem("Test/Log")]
 		private static void Log()
 		{
 			Debug.Log("hello");
 		}
-		
+
 		[MenuItem("Test/LogWarning")]
 		private static void Warning()
 		{
 			Debug.LogWarning("hello");
 		}
-		
-		
+
+
 		[MenuItem("Test/LogError")]
 		private static void Error()
 		{
 			Debug.LogError("hello");
 		}
-		
+
 		// [InitializeOnLoadMethod]
 		[MenuItem("Test/LogEmptyMessage")]
 		private static void LogEmpty()
@@ -34,7 +34,18 @@ namespace Demystify._Tests
 			Debug.Log(string.Empty);
 			Debug.LogWarning("");
 			Debug.LogError(null);
-			Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, "");
+			Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, "123");
+		}
+
+		[MenuItem("Test/Log Many")]
+		private static void LogMany()
+		{
+			for (var i = 0; i < 100; i++)
+			{
+				var fn = Path.GetRandomFileName();
+				for (var k = 0; k < 1 + (int) (Random.value * 5); k++)
+					Debug.Log(fn);
+			}
 		}
 	}
 }
