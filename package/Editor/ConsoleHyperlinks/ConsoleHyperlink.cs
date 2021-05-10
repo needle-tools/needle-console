0c350f7a47955cd24746b5e11fef51f600aa434b
Marcel Wiessler <marcel@gaisterhand.de>
2021-05-10 12:27:39 +0200

    add console hyperlink

---
+++ package/Editor/ConsoleHyperlinks/ConsoleHyperlink.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Needle
+{
+	public static class ConsoleHyperlink
+	{
+		public static string LinkTo(this string text, string link)
+		{
+			return "<a href=\"" + link + "\">" + text + "</a>";
+		}
+		
+		/// <summary>
+		/// register hyperlink clicked handler
+		/// </summary>
+		/// <param name="receiver">callback invoked when hyperlink clicked</param>
+		/// <param name="priority">higher priority is called first</param>
+		public static void RegisterClickedCallback(IHyperlinkCallbackReceiver receiver, int priority = 0)
+		{
+			if (receiver == null) return;
+			var existing = registered.FirstOrDefault(e => e.receiver == receiver);
+			if (existing.receiver != null)
+			{
+				existing.receiver = receiver;
+				existing.priority = priority;
+			}
+			else
+			{
+				registered.Add((priority, receiver));
+			}
+			dirty = true;
+		}
+
+		public static void UnregisterCallback(IHyperlinkCallbackReceiver receiver)
+		{
+			if (receiver == null) return;
+			registered.RemoveAll(e => e.receiver == receiver);
+		}
+
+		private static List<(int priority, IHyperlinkCallbackReceiver receiver)> registered = new List<(int priority, IHyperlinkCallbackReceiver receiver)>();
+		private static bool dirty;
+
+		private static void EnsureCallbacksOrdered()
+		{
+			if (!dirty) return;
+			registered = registered.OrderBy(p => p.priority).ToList();
+			dirty = false;
+		}
+
+		[InitializeOnLoadMethod]
+		private static void Init()
+		{
+			// subscribe to unity event
+			var evt = typeof(EditorGUI).GetEvent("hyperLinkClicked", BindingFlags.Static | BindingFlags.NonPublic);
+			if (evt != null)
+			{
+				var method = typeof(ConsoleHyperlink).GetMethod("OnClicked", BindingFlags.Static | BindingFlags.NonPublic);
+				if (method != null)
+				{
+					var handler = Delegate.CreateDelegate(evt.EventHandlerType, method);
+					evt.AddMethod.Invoke(null, new object[] {handler});
+				}
+			}
+			
+			// register types that implement interface
+			var implementations = TypeCache.GetTypesDerivedFrom<IHyperlinkCallbackReceiver>();
+			foreach (var t in implementations)
+			{
+				try
+				{
+					if (typeof(Object).IsAssignableFrom(t))
+					{
+						var instances = Object.FindObjectsOfType(t);
+						foreach(var inst in instances)
+							RegisterClickedCallback(inst as IHyperlinkCallbackReceiver); 
+					}
+					else
+					{
+						// skip bridge
+						if (typeof(MethodBridge).IsAssignableFrom(t)) continue;
+						
+						if(t.IsClass && !t.IsAbstract && t.GetConstructors().Any(c => c.GetParameters().Length <= 0))
+							RegisterClickedCallback(Activator.CreateInstance(t) as IHyperlinkCallbackReceiver); 
+					}
+				}
+				catch(Exception e)
+				{
+					Debug.LogException(e);
+				}
+			}
+
+			var methods = TypeCache.GetMethodsWithAttribute<HyperlinkCallback>();
+			foreach (var m in methods)
+			{
+				if (!m.IsStatic) continue;
+				var wrapper = new MethodBridge(m);
+				var attribute = m.GetCustomAttribute<HyperlinkCallback>();
+				RegisterClickedCallback(wrapper, attribute.Priority);
+			}
+		}
+
+		private static PropertyInfo property;
+
+		// ReSharper disable once UnusedMember.Local
+		private static void OnClicked(object sender, EventArgs args)
+		{
+			if (property == null)
+			{
+				property = args.GetType().GetProperty("hyperlinkInfos", BindingFlags.Instance | BindingFlags.Public);
+				if (property == null) return;
+			}
+
+			if (property.GetValue(args) is Dictionary<string, string> infos)
+			{
+				if (infos.TryGetValue("href", out var path))
+				{
+					infos.TryGetValue("line", out var line);
+					EnsureCallbacksOrdered();
+					for (var index = registered.Count - 1; index >= 0; index--)
+					{
+						var (_, receiver) = registered[index];
+						if (receiver == null || (receiver is Object obj && !obj))
+						{
+							registered.RemoveAt(index);
+							continue;
+						}
+						var res = receiver?.OnHyperlinkClicked(path, line) ?? false;
+						if (res) break;
+					}
+				}
+			}
+		}
+		
+
+		private class MethodBridge : IHyperlinkCallbackReceiver
+		{
+			private readonly MethodInfo method;
+			private bool pathOnly;
+
+			public MethodBridge(MethodInfo method)
+			{
+				this.method = method;
+				var para = this.method.GetParameters();
+				pathOnly = para.Length == 1;
+			}
+			
+			public bool OnHyperlinkClicked(string path, string line)
+			{
+				var res = pathOnly ?method?.Invoke(null, new object[]{path}) : method?.Invoke(null, new object[]{path, line});
+				if (res is bool boolResult) return boolResult;
+				return false;
+			}
+		}
+	}
+}
---
+++ package/Editor/ConsoleHyperlinks/OpenURLCallback.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+
+namespace Needle
+{
+	public class OpenURLCallback
+	{
+		[HyperlinkCallback(Priority = -1)]
+		// ReSharper disable once UnusedMember.Local
+		private static bool OnHyperlinkClicked(string path)
+		{
+			if (path.StartsWith("www."))
+			{
+				Application.OpenURL(path);
+				return true;
+			}
+				
+			var result = Uri.TryCreate(path, UriKind.Absolute, out var uriResult) 
+			             && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+				
+			if (result)
+			{
+				var url = uriResult.ToString();
+				Application.OpenURL(url);
+				return true;
+			}
+
+			
+			// if the path is not an url but some external file path open if with default app
+			if (!path.EndsWith(".cs"))
+			{
+				if (File.Exists(path))
+				{
+					var absolute = Path.GetFullPath(path);
+					var isExternalPath = !absolute.StartsWith(Application.dataPath);
+					if (isExternalPath)
+					{
+						OpenWithDefaultProgram(absolute);
+						return true;
+					}
+				}
+			}
+			
+
+			return false;
+		}
+		
+		private static void OpenWithDefaultProgram(string path)
+		{
+			var proc = new Process();
+			proc.StartInfo.FileName = "explorer";
+			proc.StartInfo.Arguments = "\"" + path + "\"";
+			proc.Start();
+		}
+	}
+}
