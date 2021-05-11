dd6f0e81e28b9dc250c9346f5362af666f63e031
Felix Herbst <felix.herbst@gmail.com>
2021-05-11 14:51:32 +0200

    rm unused usings

--- package/Editor/CustomConsole/ConsoleList.cs
+++ package/Editor/CustomConsole/ConsoleList.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
-using System.Linq;
 using UnityEditor;
-using UnityEditorInternal;
 using UnityEngine;
-using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace Needle.Demystify
 {
