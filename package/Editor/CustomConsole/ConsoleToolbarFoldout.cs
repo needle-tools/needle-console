ffc3d2a23d9e29f1a7c128e22d5c6ea8841f8036
Marcel Wiessler <marcel@gaisterhand.de>
2021-05-12 22:44:51 +0200

    popup location + tested solo "s"

--- package/Editor/CustomConsole/ConsoleToolbarFoldout.cs
+++ package/Editor/CustomConsole/ConsoleToolbarFoldout.cs
@@ -10,11 +10,11 @@ namespace Needle.Demystify
 		{
 			var rect = GUILayoutUtility.GetLastRect();
 			rect.x += rect.width;
-			var label = new GUIContent("Filter", "Console Filter Options");
+			var label = new GUIContent("Filter");
 			rect.width = EditorStyles.toolbarDropDown.CalcSize(label).x;
 			if (EditorGUI.DropdownButton(rect, label, FocusType.Keyboard, EditorStyles.toolbarDropDown))
 			{
-				PopupWindow.Show(rect, new FilterFoldoutContent(), new[] {PopupLocation.Below});
+				PopupWindow.Show(rect, new FilterFoldoutContent(), new[] {PopupLocation.BelowAlignLeft, PopupLocation.Above});
 			}
 
 			GUILayout.Space(rect.width);
