417e61deb303c0538c177c0cfd2c7677568abc39
Marcel Wiessler <marcel@gaisterhand.de>
2021-06-18 11:34:09 +0200

    Minor Comment

--- package/Runtime/Extensions/DebugEditor.cs
+++ package/Runtime/Extensions/DebugEditor.cs
@@ -6,8 +6,7 @@ namespace Needle.Console
 {
 	/// <summary>
 	/// Class to log only in Unity editor, double clicking console logs produced by this class still open the calling source file)
-	/// 
-	/// NOTE: Implement your own version of this is supported. Just implement a class named "DebugEditor" and any method inside this class starting with "Log" will, when double clicking logs produced by that method, open the calling file
+	/// NOTE: Implement your own version of this is supported. Just implement a class named "DebugEditor" and any method inside this class starting with "Log" will, when double clicked, open the file of the calling method. Use [Conditional] attributes to control when any of these methods should be included.
 	/// </summary>
 	public static class DebugEditor
 	{
