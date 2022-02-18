5ca33058ae95f0f0dba42f7a1e622d3124ee4229
Marcel Wiessler <marcel@gaisterhand.de>
2022-02-18 23:36:41 +0100

    remove regex in hyperlink fix

--- package/Editor/Modules/Hyperlinks.cs
+++ package/Editor/Modules/Hyperlinks.cs
@@ -9,7 +9,7 @@ namespace Needle.Console
 	internal static class Hyperlinks
 	{
 		private static readonly Regex hyperlinks = new Regex(@"((?<brackets>\))?(?<prefix> in) (?<file>.*?):line (?<line>\d+)(?<post>.*))",
-			RegexOptions.Compiled | RegexOptions.Multiline);
+			RegexOptions.Compiled | RegexOptions.Singleline);
 
 		private static readonly StringBuilder stacktraceBuilder = new StringBuilder();
 		private static readonly StringBuilder fixStacktraceBuilder = new StringBuilder();
@@ -25,7 +25,7 @@ namespace Needle.Console
 				fixStacktraceBuilder.Clear();
 				var line = t;
 				// hyperlinks capture 
-				var path = Fix(ref line);
+				var path = Fix(line, fixStacktraceBuilder, out var lineNumber);
 				if (!string.IsNullOrEmpty(path))
 				{
 					// dont append path to editor only lines to force unity to open the previous file path
@@ -34,8 +34,8 @@ namespace Needle.Console
 					}
 					else
 					{
-						path = path.Replace("\n", "");
-						fixStacktraceBuilder.Append(")").Append(path);
+						// path = path.Replace("\n", "");
+						fixStacktraceBuilder.Append(path).Append(lineNumber).Append(")");
 						line = fixStacktraceBuilder.ToString();
 						Filepaths.TryMakeRelative(ref line);
 					}
@@ -55,36 +55,53 @@ namespace Needle.Console
 		/// </summary>
 		/// <param name="line"></param>
 		/// <returns>hyperlink that must be appended to line once further processing is done</returns>
-		public static string Fix(ref string line)
+		private static string Fix(string line, StringBuilder lineBuilder, out string lineNumber)
 		{
-			var match = hyperlinks.Match(line);
-			lineBuilder.Clear();
-			if (match.Success)
-			{
-				var file = match.Groups["file"];
-				var lineNr = match.Groups["line"];
-				var brackets = match.Groups["brackets"].ToString();
-				if (string.IsNullOrEmpty(brackets)) brackets = ")";
-				var post = match.Groups["post"];
-
-				// var end = line.Substring(match.Index, line.Length - match.Index);
-				// line = line.Remove(match.Index, end.Length) + brackets;
-				// var newString = " (at " + file + ":" + lineNr + ")\n";
-				// if (post.Success)
-				// 	newString += post;
-				// return newString;
-
-				var end = line.Substring(match.Index, line.Length - match.Index);
-				line = line.Remove(match.Index, end.Length);
-				fixStacktraceBuilder.Append(line).Append(brackets);
-				
-				lineBuilder.Append(" (at ").Append(file).Append(":").Append(lineNr).Append(")\n");
-				if (post.Success) 
-					lineBuilder.Append(post);
-				return lineBuilder.ToString();
-			}
-
-			return string.Empty;
+			lineNumber = string.Empty;
+			
+			const string START = " in ";
+			const string END = "line ";
+			var begin = line.LastIndexOf(START, StringComparison.Ordinal);
+			var end = line.LastIndexOf(END, StringComparison.Ordinal);
+			if (begin < 0 || end < 0) return string.Empty;
+			begin += START.Length;
+			var pathLength = end - begin;
+			var lineStart = end + END.Length;
+			lineNumber = line.Substring(lineStart, line.Length - lineStart);
+			var path = line.Substring(begin, pathLength);
+			line = line.Substring(0, begin - START.Length);
+			lineBuilder.Append(line).Append(") (at ");
+			return path;
+			
+			
+			// var match = hyperlinks.Match(line);
+			// Hyperlinks.lineBuilder.Clear();
+			// if (match.Success)
+			// {
+			// 	var file = match.Groups["file"];
+			// 	var lineNr = match.Groups["line"];
+			// 	var brackets = match.Groups["brackets"].ToString();
+			// 	if (string.IsNullOrEmpty(brackets)) brackets = ")";
+			// 	var post = match.Groups["post"];
+			//
+			// 	// var end = line.Substring(match.Index, line.Length - match.Index);
+			// 	// line = line.Remove(match.Index, end.Length) + brackets;
+			// 	// var newString = " (at " + file + ":" + lineNr + ")\n";
+			// 	// if (post.Success)
+			// 	// 	newString += post;
+			// 	// return newString;
+			//
+			// 	var end = line.Substring(match.Index, line.Length - match.Index);
+			// 	line = line.Remove(match.Index, end.Length);
+			// 	lineBuilder.Append(line).Append(brackets);
+			// 	
+			// 	Hyperlinks.lineBuilder.Append(" (at ").Append(file).Append(":").Append(lineNr).Append(")\n");
+			// 	if (post.Success) 
+			// 		Hyperlinks.lineBuilder.Append(post);
+			// 	return Hyperlinks.lineBuilder.ToString();
+			// }
+			//
+			// return string.Empty;
 		}
 
 
