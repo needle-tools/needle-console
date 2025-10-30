ab91dc73a99075717d0b64d0e410f50efee2fc49
C <cmann1@users.noreply.github.com>
2025-10-30 13:07:04 +0200

    Add Unity version check for stack trace indentation

--- package/Editor/NeedleConsole.cs
+++ package/Editor/NeedleConsole.cs
@@ -124,7 +124,11 @@ namespace Needle.Console
 							SyntaxHighlighting.AddSyntaxHighlighting(ref line);
 
 						// Indent wrapped lines.
+#if UNITY_6000_0_OR_NEWER
 						var l = "<indent=2em><line-indent=-2em>" + line.Trim() + "</line-indent></indent>";
+#else
+						var l = line.Trim();
+#endif
 						if (!string.IsNullOrEmpty(l))
 						{
 							if (!l.EndsWith("\n"))
