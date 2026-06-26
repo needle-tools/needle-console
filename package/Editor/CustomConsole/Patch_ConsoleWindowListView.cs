ce4bf5c12dee1a9b81d7967f436bd32488c67e77
Robert Dorn <mich@robertdorn.de>
2026-06-26 14:09:47 +0200

    fixed Unity 6.4+ compatibility

--- package/Editor/CustomConsole/Patch_ConsoleWindowListView.cs
+++ package/Editor/CustomConsole/Patch_ConsoleWindowListView.cs
@@ -22,109 +22,101 @@ namespace Needle.Console
 		private static IEnumerable<CodeInstruction> Transpiler(MethodBase method, ILGenerator il, IEnumerable<CodeInstruction> instructions)
 		{
 #if !NETSTANDARD
-				var skipLabel = il.DefineLabel();
-				var arr = instructions.ToArray();
-				var loadListViewElementIndex = -1;
+			var skipLabel = il.DefineLabel();
+			var arr = instructions.ToArray();
+			var loadListViewElementIndex = -1;
 
-				for (var index = 0; index < arr.Length; index++)
+			// The insertion points used to be hard-coded instruction indices per Unity version.
+			// Those indices shift whenever UnityEditor.ConsoleWindow.OnGUI changes and silently
+			// broke patching on Unity 6.4+ with an InvalidProgramException (the OnDrawList guard
+			// ended up at the wrong stack position). We now locate the insertion points by their
+			// surrounding IL instead, so the patch keeps working across Unity versions.
+			var foldoutIndex = -1; // before GUILayout.FlexibleSpace
+			var toolbarIndex = -1; // before GUILayout.EndHorizontal
+			var listIndex = -1; // before the SplitterGUILayout.BeginVerticalSplit(spl) block
+#if UNITY_2020_1_OR_NEWER
+			for (var i = 0; i < arr.Length; i++)
+			{
+				if (arr[i].opcode != OpCodes.Call || !(arr[i].operand is MethodInfo cm))
+					continue;
+
+				if (foldoutIndex == -1 && cm.Name == "FlexibleSpace" && cm.DeclaringType?.Name == "GUILayout")
+					foldoutIndex = i;
+				else if (toolbarIndex == -1 && cm.Name == "EndHorizontal" && cm.DeclaringType?.Name == "GUILayout")
+					toolbarIndex = i;
+				else if (listIndex == -1 && cm.Name == "BeginVerticalSplit" && cm.DeclaringType?.Name == "SplitterGUILayout")
 				{
-					var inst = arr[index];
-					
-					// UnityEngine.Debug.Log("<color=grey>" + index + ": " + inst + "</color>"); 
-
-					// get local index for current list view element
-					if (loadListViewElementIndex == -1 || inst.IsStloc() && inst.operand is LocalBuilder)
+					// walk back to the start of the call's arguments ("ldarg.0; ldfld spl; ...") so the
+					// inserted guard does not corrupt the stack used to push BeginVerticalSplit's arguments
+					for (var k = i - 1; k > 0; k--)
 					{
-						var loc = inst.operand as LocalBuilder;
-						if (loc?.LocalType == typeof(ListViewElement))
-							loadListViewElementIndex = loc.LocalIndex;
-					}
-
-					if (inst.opcode == OpCodes.Call && inst.operand is MethodInfo m) 
-					{
-						if (m.DeclaringType == typeof(LogEntries) && m.Name == "GetLinesAndModeFromEntryInternal")
+						if (arr[k].opcode == OpCodes.Ldfld && arr[k].operand is FieldInfo f && f.Name == "spl" && arr[k - 1].IsLdarg(0))
 						{
-							yield return inst;
-							// load text is one element before
-							var ldStr = arr[index - 1];
-							yield return new CodeInstruction(OpCodes.Ldloc, loadListViewElementIndex);
-							yield return ldStr;
-							yield return CodeInstruction.Call(typeof(ConsoleTextPrefix), nameof(ConsoleTextPrefix.ModifyText));
-							continue;
+							listIndex = k - 1;
+							break;
 						}
 					}
-
-					if (inst.opcode == OpCodes.Ret)
-					{
-						inst.labels.Add(skipLabel);
-					}
-					
-					// this is before "GUILayout.FlexibleSpace"
-					// https://github.com/Unity-Technologies/UnityCsReference/blob/61f92bd79ae862c4465d35270f9d1d57befd1761/Editor/Mono/ConsoleWindow.cs#L539
-#if UNITY_6000_2_OR_NEWER
-					if (index == 233)
-#elif UNITY_6000_0_OR_NEWER || UNITY_2022_3_OR_NEWER
-					if (index == 240)
-#elif UNITY_2022_1_OR_NEWER
-					if (index == 30)
-#elif UNITY_2021_1_OR_NEWER
-					if (index == 172)
-#elif UNITY_2020_1_OR_NEWER
-					if (index == 189)
-#else // 2019
-					if (index == -1)
-#endif
-					{
-						yield return CodeInstruction.Call(typeof(ConsoleToolbarFoldout), nameof(ConsoleToolbarFoldout.OnDrawFoldouts)); 
-					}
-					
-					// this is before "EndHorizontal"
-					// https://github.com/Unity-Technologies/UnityCsReference/blob/4d031e55aeeb51d36bd94c7f20182978d77807e4/Editor/Mono/ConsoleWindow.cs#L600
-#if UNITY_6000_2_OR_NEWER
-					if (index == 342)
-#elif UNITY_6000_0_OR_NEWER || UNITY_2022_3_OR_NEWER
-					if (index == 349)
-#elif UNITY_2022_1_OR_NEWER
-					if (index == 200)
-#elif UNITY_2021_1_OR_NEWER
-					if (index == 329)
-#elif UNITY_2020_1_OR_NEWER
-					if (index == 317)
-#else // 2019
-					if (index == -1)
-#endif
-					{
-						yield return CodeInstruction.Call(typeof(ConsoleToolbarIcon), nameof(ConsoleToolbarIcon.OnDrawToolbar));
-					}
-
-				// TODO: properly search for the right spots
-				// this is right before  SplitterGUILayout.BeginVerticalSplit(spl);
-#if UNITY_6000_3_OR_NEWER
-					if (index == 394)
-#elif UNITY_6000_2_OR_NEWER
-					if (index == 380)
-					// if (index == 374) // Unity < 6000.2.6
-#elif UNITY_6000_0_OR_NEWER || UNITY_2022_3_OR_NEWER
-					if (index == 382)
-#elif UNITY_2022_1_OR_NEWER
-					if (index == 2)
-#elif UNITY_2021_1_OR_NEWER
-					if (index == 330) 
-#elif UNITY_2020_3_38_OR_NEWER
-					if (index == 323)
-#elif UNITY_2020_1_OR_NEWER
-					if (index == 318)
-#else // 2019
-				if (index == -1)
-#endif
-					{
-						yield return new CodeInstruction(OpCodes.Ldarg_0);
-						yield return CodeInstruction.Call(typeof(ConsoleList), nameof(ConsoleList.OnDrawList));
-						yield return new CodeInstruction(OpCodes.Brfalse, skipLabel);
-					}
-					
-					yield return inst;
 				}
+			}
+#endif
+
+			for (var index = 0; index < arr.Length; index++)
+			{
+				var inst = arr[index];
+
+				// UnityEngine.Debug.Log("<color=grey>" + index + ": " + inst + "</color>");
+
+				// get local index for current list view element
+				if (loadListViewElementIndex == -1 || inst.IsStloc() && inst.operand is LocalBuilder)
+				{
+					var loc = inst.operand as LocalBuilder;
+					if (loc?.LocalType == typeof(ListViewElement))
+						loadListViewElementIndex = loc.LocalIndex;
+				}
+
+				if (inst.opcode == OpCodes.Call && inst.operand is MethodInfo m)
+				{
+					if (m.DeclaringType == typeof(LogEntries) && m.Name == "GetLinesAndModeFromEntryInternal")
+					{
+						yield return inst;
+						// load text is one element before
+						var ldStr = arr[index - 1];
+						yield return new CodeInstruction(OpCodes.Ldloc, loadListViewElementIndex);
+						yield return ldStr;
+						yield return CodeInstruction.Call(typeof(ConsoleTextPrefix), nameof(ConsoleTextPrefix.ModifyText));
+						continue;
+					}
+				}
+
+				if (inst.opcode == OpCodes.Ret)
+				{
+					inst.labels.Add(skipLabel);
+				}
+
+				// this is before "GUILayout.FlexibleSpace"
+				// https://github.com/Unity-Technologies/UnityCsReference/blob/61f92bd79ae862c4465d35270f9d1d57befd1761/Editor/Mono/ConsoleWindow.cs#L539
+				if (index == foldoutIndex)
+				{
+					yield return CodeInstruction.Call(typeof(ConsoleToolbarFoldout), nameof(ConsoleToolbarFoldout.OnDrawFoldouts));
+				}
+
+				// this is before "EndHorizontal"
+				// https://github.com/Unity-Technologies/UnityCsReference/blob/4d031e55aeeb51d36bd94c7f20182978d77807e4/Editor/Mono/ConsoleWindow.cs#L600
+				if (index == toolbarIndex)
+				{
+					yield return CodeInstruction.Call(typeof(ConsoleToolbarIcon), nameof(ConsoleToolbarIcon.OnDrawToolbar));
+				}
+
+				// this is right before  SplitterGUILayout.BeginVerticalSplit(spl);
+				if (index == listIndex)
+				{
+					yield return new CodeInstruction(OpCodes.Ldarg_0);
+					yield return CodeInstruction.Call(typeof(ConsoleList), nameof(ConsoleList.OnDrawList));
+					yield return new CodeInstruction(OpCodes.Brfalse, skipLabel);
+				}
+
+				yield return inst;
+			}
 #else
 				if (SessionState.GetBool("NeedleConsole:NetStandardIsUnsupportedWarning", false) == false)
 			{
@@ -134,29 +126,5 @@ namespace Needle.Console
 			return instructions;
 #endif
 		}
-
-
-
-		// private class Instructor
-		// {
-		// 	public List<Func<CodeInstruction, bool>> List = new List<Func<CodeInstruction, bool>>();
-		// 	public Func<CodeInstruction, bool> OnFound;
-		//
-		// 	private int currentListIndex;
-		// 	private int firstIndex;
-		// 	
-		// 	public void Check(CodeInstruction inst, int index)
-		// 	{
-		// 		var check = List[currentListIndex];
-		// 		if (check(inst))
-		// 		{
-		// 			currentListIndex += 1;
-		// 			if (currentListIndex >= List.Count)
-		// 			{
-		// 				OnFound?.Invoke()
-		// 			}
-		// 		}
-		// 	}
-		// }
 	}
 }
